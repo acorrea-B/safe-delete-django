442adb257eca97748a3befc8fe6b1d66911bc3f4
Janier Alejandro Banguera Correa <jbanguera@tpaga.co>
2021-12-02 11:26:57 -0500

    add new test to relation validation

--- cars/tests.py
+++ cars/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Line2
+from .models import Line2, Car2
 from django.db import models
 
 class Line2TestCase(TestCase):
@@ -19,12 +19,8 @@ class Line2TestCase(TestCase):
         line.delete()
         self.assertIsNotNone(line.deleted_at)
         self.assertEqual(len(Line2.objects.all()), 2)
-    
-    def test_deleted_line(self):
-        line = Line2.objects.get(pk = 1)
-        line.delete()
         with self.assertRaises(Line2.DoesNotExist):
-            Line2.objects.get(pk = 1)
+            Line2.objects.get(pk = 1)   
 
     def test_get_deleted_items(self):
         line = Line2.objects.get(pk = 1)
@@ -50,3 +46,14 @@ class Line2TestCase(TestCase):
         line.delete()
         lines = Line2.objects.filter(name__contains="S")
         self.assertEqual(len(lines), 2)
+
+    def test_get_car_deleted_line(self):
+        line = Line2.objects.get(pk = 1)
+        car = Car2(name = "Subaru")
+        car.save()
+        car.line.add(line)
+        self.assertEqual(line.name, car.line.first().name)
+        line.delete()
+        car_deleted_account = Car2.objects.\
+            get(pk = car.id)
+        self.assertIsNone(car.line.first())
